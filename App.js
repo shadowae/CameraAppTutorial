5355b71d44c94cd3e744005948c44e71cc5ac037
Satish K <satish.appanasamy@gmail.com>
2023-04-29 12:32:51 +1000

    init

--- App.js
+++ App.js
@@ -1,12 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, SafeAreaView, Button, Image } from 'react-native';
+import { TouchableOpacity, StyleSheet, Text, View, SafeAreaView, Button, Image } from 'react-native';
 import { useEffect, useRef, useState } from 'react';
+import * as ImagePicker from 'expo-image-picker';
 import { Camera } from 'expo-camera';
 import { shareAsync } from 'expo-sharing';
 import * as MediaLibrary from 'expo-media-library';
 
 export default function App() {
   let cameraRef = useRef();
+  const [image, setImage] = useState(null);
   const [hasCameraPermission, setHasCameraPermission] = useState();
   const [hasMediaLibraryPermission, setHasMediaLibraryPermission] = useState();
   const [photo, setPhoto] = useState();
@@ -19,6 +21,54 @@ export default function App() {
       setHasMediaLibraryPermission(mediaLibraryPermission.status === "granted");
     })();
   }, []);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await Camera.requestCameraPermissionsAsync();
+  //     if (status !== 'granted') {
+  //       alert('Sorry, we need camera permissions to make this work!');
+  //     }
+  //   })();
+  // }, []);
+
+  // useEffect(() => {
+  //   (async () => {
+  //     const { status } = await Permissions.askAsync(Permissions.MEDIA_LIBRARY);
+  //     if (status !== 'granted') {
+  //       alert('Sorry, we need camera roll permissions to make this work!');
+  //     }
+  //   })();
+
+  // }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log('result', result.assets[0].uri);
+
+    if (!result.cancelled) {
+      setImage(result.assets[0].uri);
+    }
+  };
+  const pickImageFromLib = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log('result', result.assets[0]);
+
+    if (!result.cancelled) {
+      setImage(result.assets[0].uri);
+    }
+  };
 
   if (hasCameraPermission === undefined) {
     return <Text>Requesting permissions...</Text>
@@ -37,51 +87,89 @@ export default function App() {
     setPhoto(newPhoto);
   };
 
-  if (photo) {
-    let sharePic = () => {
-      shareAsync(photo.uri).then(() => {
-        setPhoto(undefined);
-      });
-    };
+  // if (photo) {
+  //   let sharePic = () => {
+  //     shareAsync(photo.uri).then(() => {
+  //       setPhoto(undefined);
+  //     });
+  //   };
 
-    let savePhoto = () => {
-      MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
-        setPhoto(undefined);
-      });
-    };
+  //   let savePhoto = () => {
+  //     MediaLibrary.saveToLibraryAsync(photo.uri).then(() => {
+  //       setPhoto(undefined);
+  //     });
+  //   };
 
-    return (
-      <SafeAreaView style={styles.container}>
-        <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
-        <Button title="Share" onPress={sharePic} />
-        {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
-        <Button title="Discard" onPress={() => setPhoto(undefined)} />
-      </SafeAreaView>
-    );
-  }
+  //   return (
+  //     <SafeAreaView style={styles.container}>
+  //       <Image style={styles.preview} source={{ uri: "data:image/jpg;base64," + photo.base64 }} />
+  //       <Button title="Share" onPress={sharePic} />
+  //       {hasMediaLibraryPermission ? <Button title="Save" onPress={savePhoto} /> : undefined}
+  //       <Button title="Discard" onPress={() => setPhoto(undefined)} />
+  //     </SafeAreaView>
+  //   );
+  // }
 
   return (
-    <Camera style={styles.container} ref={cameraRef}>
-      <View style={styles.buttonContainer}>
-        <Button title="Take Pic" onPress={takePic} />
+    <View style={styles.container}>
+      <View style={styles.header}>
+        <Text style={styles.title}>Celebrity Doppelganger Finder</Text>
       </View>
-      <StatusBar style="auto" />
-    </Camera>
+      <View style={styles.main}>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={pickImage}>
+          <Text style={styles.buttonText}>Take Photo</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={pickImageFromLib}>
+          <Text style={styles.buttonText}>Upload Photo</Text>
+        </TouchableOpacity>
+        {image && (
+          <Image
+            source={{ uri: image }}
+            style={styles.photo}
+          />
+        )}
+      </View>
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff',
+  },
+  header: {
+    backgroundColor: '#fff',
+    padding: 40,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    textAlign: 'center',
+  },
+  main: {
+    flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
   },
-  buttonContainer: {
-    backgroundColor: '#fff',
-    alignSelf: 'flex-end'
+  button: {
+    backgroundColor: '#e74c3c',
+    padding: 10,
+    borderRadius: 10,
+    margin: 10,
   },
-  preview: {
-    alignSelf: 'stretch',
-    flex: 1
-  }
+  buttonText: {
+    color: '#fff',
+    fontSize: 20,
+  },
+  photo: {
+    width: 200,
+    height: 200,
+    borderRadius: 100,
+    margin: 10,
+  },
 });
